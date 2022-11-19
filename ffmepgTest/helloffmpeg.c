1e89b101ce6078fa23b1fac2e86ae7019ecbcd7b
yuankang <troyyuan@gmail.com>
2022-11-19 19:21:19 +0800

    rm parse*/

--- ffmepgTest/helloffmpeg.c
+++ ffmepgTest/helloffmpeg.c
@@ -1,12 +1,10 @@
-// gcc ffmpegHi.c -lavcodec
-
-// make examples
-
+// gcc helloffmpeg.c -lavcodec
 #include <stdio.h>
 #include <libavcodec/avcodec.h>
 
 int main(int argc, char const *argv[])
 {
     printf("%s\n", avcodec_configuration());
+    printf("hello ffmpeg\n");
     return 0;
 }
